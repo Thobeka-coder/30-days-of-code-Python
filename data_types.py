72c6d3369110834a67f81070da15b50ebfc9a690
Thobeka <thobekasithole3@gmail.com>
2021-01-29 03:21:10 -0400

    completed successfully

---
+++ data_types.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+it = int(12)
+db = float(4)
+sr = str("is the best place to learn and practice coding!")
+# Read and save an integer, double, and String to your variables.
+it = int(input())
+db = float(input())
+sr = input()
+# Print the sum of both integer variables on a new line.
+print(i + it)
+# Print the sum of the double variables on a new line.
+print(d + db)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + sr)
